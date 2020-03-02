bc748ed3eebe5fc04272485f70c2e5b876a62b2a
loolmba <61137783+loolmba@users.noreply.github.com>
2020-03-02 11:21:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,6 @@ class Quiestion():
         if right_answer not in answer_list:
             print('Right answer not in answers list!')
 
-    self.question
-    self.answers = []
-    self.right_answer
 
 
 class Quiz():
@@ -21,6 +18,26 @@ class Quiz():
     def add_question(self, ques_name, answer_list, right_answer):
         new_ques = Quiestion(ques_name, answer_list, right_answer)
         self.questions.append(new_ques)
+
+    def get_quiz_from_file(self, file):
+        f = open(filename, 'r')
+        quiz = f.read()
+        for line in quiz:
+            tmp_ques=[]
+            tmp.line.split('*')
+        tmp = []
+        tmp_ques = tmp[0]
+        answers = tmp[1].split('')
+        answers = []
+        right_ans = answers[len(answers)-1]
+        answers.pop()
+        ques=Quiestion(tmp_ques,answers,right_ans)
+        self.questions.append(ques)
+
+
+
+        f.close()
+
 
 
 
